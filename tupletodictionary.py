3c338aed7522af45a4ee028c07a0461cc8ef45ec
Gokuls437 <gokul.s@schnellenergy.com>
2022-12-01 17:49:12 +0530

    add script list additionally

---
+++ tupletodictionary.py
@@ -0,0 +1,27 @@
+# t=(1,2,'gokul',2000)
+# d=('num','number','name','year')
+# e=dict(zip(d,t))
+# print(e)
+#
+#
+#
+#
+# t=(1,2,'gokul',2000)
+# d=('num','number','name','year')
+# dictionary={}
+# for i in range(0,4):
+#     dictionary[t[i]]=d[i]
+# print(dictionary)
+
+
+
+
+dict={}
+dict['key']=2000
+print(dict)
+
+
+Lis = [1,2,3,4,5,6,9,8,7]
+print(Lis)
+
+
